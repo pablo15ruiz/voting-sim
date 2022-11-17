dec3c07d3295d4c9aeb3b32f349d2c244c39c95c
Pablo Ruiz <57796883+pablo15ruiz@users.noreply.github.com>
2022-11-17 13:40:48 +0100

    Update voter_models.py

--- voter_models.py
+++ voter_models.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 
-from numpy.random import random_sample
+import numpy as np
+from numpy.random import choice, normal, random_sample, shuffle
+from scipy.spatial import distance_matrix
 
 class VoterModel(ABC):
 
@@ -18,38 +20,32 @@ class ClusteredSpatialModel(VoterModel):
 
     def generate_utilities(self, n_vot, n_cand):
         voters = self.generate_voters(n_vot, n_dim=5)
-        candidates = self.generate_candidates(n_cand)
+        candidates = self.generate_candidates(voters, n_cand, n_dim=5)
         utilities = distance_matrix(voters, candidates)
         return 1 - utilities/utilities.max()
 
     def generate_voters(self, n_vot, n_dim):
         voters = np.empty((n_vot, n_dim))
-        n_dim_per_view = chinese_restaurant_process(n_dim)
+        n_dim_per_view = self.chinese_restaurant_process(n_dim)
         views = np.repeat(range(len(n_dim_per_view)), n_dim_per_view)
-        np.random.shuffle(views)
-        for view_idx, n_dim in enumerate(n_dim_per_view):
-            n_voters_per_cluster = chinese_restaurant_process(n_vot)
-            clusters = np.repeat(range(len(n_voters_per_cluster)), n_voters_per_cluster)
-            np.random.shuffle(clusters)
-            for cluster_idx, n_voters in enumerate(n_voters_per_cluster):
-                for dim in range(n_dim):
-                    parameters = np.random.rand(2)
-                    cluster_voters = np.random.normal(
-                        parameters[0],
-                        parameters[1],
-                        n_voters
-                    )
-                    voter_idx = [idx for idx, c in enumerate(clusters) if c == cluster_idx]
-                    dim_idx = [idx for idx, v in enumerate(views) if v == view_idx][dim]
-                    voters[voter_idx, dim_idx] = cluster_voters
+        shuffle(views)
+        for view_idx, d in enumerate(n_dim_per_view):
+            n_vot_per_cluster = self.chinese_restaurant_process(n_vot)
+            clusters = np.repeat(range(len(n_vot_per_cluster)), n_vot_per_cluster)
+            shuffle(clusters)
+            for cluster_idx, v in enumerate(n_vot_per_cluster):
+                param = random_sample(2)
+                cluster_voters = normal(param[0], param[1], (v, d))
+                voter_idx = np.argwhere(clusters == cluster_idx)
+                dim_idx = np.flatnonzero(views == view_idx)
+                voters[voter_idx, dim_idx] = cluster_voters
 
-    def generate_candidates(self, n_cand, n_dim):
+        return voters
+
+    def generate_candidates(self, voters, n_cand, n_dim):
         mean = voters.mean(axis=0)
         std = voters.std(axis=0)
-        candidates = np.random.random_sample(
-            (self.n_candidates, n_dim)
-        )
-        candidates = 2*std*candidates + mean - std
+        return 2*std*random_sample((n_cand, n_dim)) + mean - std
 
     @staticmethod
     def chinese_restaurant_process(n):
@@ -58,7 +54,7 @@ class ClusteredSpatialModel(VoterModel):
             for customer in range(2, n+1):
                 p = tables/customer
                 p[-1] = 1/customer
-                table_choice = np.random.choice(n_tables+1, p=p)
+                table_choice = choice(n_tables+1, p=p)
                 tables[table_choice] += 1
                 if table_choice == n_tables:
                     tables = np.append(tables, 0)
