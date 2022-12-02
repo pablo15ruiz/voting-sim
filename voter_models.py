942e89dcbeb8c0a7612292187dda0d49d9e63537
Pablo Ruiz <57796883+pablo15ruiz@users.noreply.github.com>
2022-12-02 20:14:18 +0100

    Update voter_models.py

--- voter_models.py
+++ voter_models.py
@@ -4,11 +4,13 @@ import numpy as np
 from numpy.random import choice, normal, random_sample, shuffle
 from scipy.spatial import distance_matrix
 
+
 class VoterModel(ABC):
 
     @abstractmethod
     def generate_utilities(self, n_vot, n_cand):
         pass
+
 
 class ImpartialCulture(VoterModel):
 
@@ -19,12 +21,12 @@ class ImpartialCulture(VoterModel):
 class ClusteredSpatialModel(VoterModel):
 
     def generate_utilities(self, n_vot, n_cand):
-        voters = self.generate_voters(n_vot, n_dim=5)
-        candidates = self.generate_candidates(voters, n_cand, n_dim=5)
-        utilities = distance_matrix(voters, candidates)
-        return 1 - utilities/utilities.max()
+        voters = self.generate_voters(n_vot)
+        candidates = self.generate_candidates(voters, n_cand)
+        utilities = self.compute_utilities(voters, candidates)
+        return utilities
 
-    def generate_voters(self, n_vot, n_dim):
+    def generate_voters(self, n_vot, n_dim=2):
         voters = np.empty((n_vot, n_dim))
         n_dim_per_view = self.chinese_restaurant_process(n_dim)
         views = np.repeat(range(len(n_dim_per_view)), n_dim_per_view)
@@ -42,10 +44,14 @@ class ClusteredSpatialModel(VoterModel):
 
         return voters
 
-    def generate_candidates(self, voters, n_cand, n_dim):
+    def generate_candidates(self, voters, n_cand, n_dim=2):
         mean = voters.mean(axis=0)
         std = voters.std(axis=0)
         return 2*std*random_sample((n_cand, n_dim)) + mean - std
+
+    def compute_utilities(self, voters, candidates):
+        utilities = distance_matrix(voters, candidates)
+        return 1 - utilities/utilities.max()
 
     @staticmethod
     def chinese_restaurant_process(n):
