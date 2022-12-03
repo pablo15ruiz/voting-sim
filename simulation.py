db55fa2bfe32b8b0d9d58f1c8e88ed7fdbad2926
Pablo Ruiz <57796883+pablo15ruiz@users.noreply.github.com>
2022-12-03 18:46:30 +0100

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -31,7 +31,7 @@ class Election:
         return winner
 
     def do_poll(self, threshold=0.7):
-        return (self.util > threshold).sum(axis=0)
+        return (self.util > threshold).sum(axis=0).argsort()
 
     def plot(self, voters, candidates):
         fig, ax = plt.subplots(figsize=(5, 5))
@@ -58,22 +58,17 @@ class Simulation:
 
         self.total_util = []
         self.total_winners = []
+        self.n_condorcet_cycles = 0
 
     def simulate(self):
         winners = []
         poll = any([method.behaviour != 'honest' for method in self.methods])
         for _ in range(self.n_iter):
-            util = self.voter_model.generate_util(
-                self.n_vot,
-                self.n_cand
-            )
+            util = self.voter_model.generate_util(self.n_vot, self.n_cand)
             self.total_util.append(util.sum(axis=0))
-            election = Election(
-                self.n_vot,
-                self.n_cand,
-                util,
-                poll=poll
-            )
+            election = Election(self.n_vot, self.n_cand, util, poll=poll)
+            if election.compute_condorcet_winner() == None:
+                self.n_condorcet_cycles += 1
             election_winners = []
             for method in self.methods:
                 winner = election.compute_winner(method)
@@ -83,16 +78,25 @@ class Simulation:
         self.total_winners = np.array(winners)
         self.total_util = np.array(self.total_util)
 
-    def compute_metrics(self):
+    def results(self):
+        print(self)
+        print(f'#Condorcet cycles: {self.n_condorcet_cycles/self.n_iter} ')
         optimum = self.total_util.max(axis=1).mean()
         idx = np.tile(range(self.n_iter), reps=(len(self.methods), 1)).T
         achieved = self.total_util[idx, self.total_winners].mean(axis=0)
         average = self.total_util.mean()
         self.vse = (achieved - average)/(optimum - average)
 
-    def show_results(self):
-        names = [f'{method} {method.behaviour}' for method in self.methods]
+        names = [repr(method) for method in self.methods]
         d = dict(zip(names, np.round(100*self.vse, 2)))
         d = {k: v for k, v in sorted(d.items(), key=lambda item: item[1])}
         for name, metric in d.items():
             print(metric, name)
+
+    def __repr__(self):
+        m = f'Simulation with {len(self.methods)} voting methods.\n'
+        m += f'Voter model: {self.voter_model}\n'
+        m += f'#Voters: {self.n_vot}\n'
+        m += f'#Candidates: {self.n_cand}\n'
+        m += f'#Iterations: {self.n_iter}\n'
+        return m
