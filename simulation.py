7a2623fa305ca89e271463b3ee72c859b587108b
Pablo Ruiz <57796883+pablo15ruiz@users.noreply.github.com>
2022-12-04 13:59:10 +0100

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,9 +1,5 @@
 import string
-
 import numpy as np
-from numpy.random import choice
-
-import matplotlib.pyplot as plt
 
 class Election:
 
@@ -12,15 +8,12 @@ class Election:
         self.n_cand = n_cand
         self.util = util
         self.poll = self.do_poll() if poll else None
-        cw = self.compute_condorcet_winner()
+        self.cw = self.compute_condorcet_winner()
 
     def compute_winner(self, method):
         poll = self.poll if method.behaviour != 'honest' else None
         ballots = method.compute_ballots(self.util, poll)
         winner = method.compute_winner(ballots)
-        if winner == None:
-            winner = choice(range(self.n_cand))
-
         return winner
 
     def compute_condorcet_winner(self):
@@ -58,42 +51,52 @@ class Simulation:
 
         self.total_util = []
         self.total_winners = []
-        self.n_condorcet_cycles = 0
+        self.cycle_freq = None
 
     def simulate(self):
+        n_cycles = 0
         winners = []
         poll = any([method.behaviour != 'honest' for method in self.methods])
         for _ in range(self.n_iter):
             util = self.voter_model.generate_util(self.n_vot, self.n_cand)
             self.total_util.append(util.sum(axis=0))
-            election = Election(self.n_vot, self.n_cand, util, poll=poll)
-            if election.compute_condorcet_winner() == None:
-                self.n_condorcet_cycles += 1
-            election_winners = []
-            for method in self.methods:
-                winner = election.compute_winner(method)
-                election_winners.append(winner)
-            winners.append(election_winners)
+            elec = Election(self.n_vot, self.n_cand, util, poll=poll)
+            n_cycles += elec.cw is None
+            w = [elec.compute_winner(method) for method in self.methods]
+            winners.append(w)
 
         self.total_winners = np.array(winners)
         self.total_util = np.array(self.total_util)
+        self.cycle_freq = n_cycles/self.n_iter
 
-    def results(self):
-        print(self)
-        print(f'#Condorcet cycles: {self.n_condorcet_cycles/self.n_iter} ')
+        self.compute_metrics()
+
+    def compute_metrics(self):
         optimum = self.total_util.max(axis=1).mean()
         idx = np.tile(range(self.n_iter), reps=(len(self.methods), 1)).T
         achieved = self.total_util[idx, self.total_winners].mean(axis=0)
         average = self.total_util.mean()
         self.vse = (achieved - average)/(optimum - average)
 
-        names = [repr(method) for method in self.methods]
+    def results(self):
+        print(self)
+        print(f'Condorcet cycle frequency: {np.round(100*self.cycle_freq, 3)}%')
+        names = [str(method) for method in self.methods]
         d = dict(zip(names, np.round(100*self.vse, 2)))
         d = {k: v for k, v in sorted(d.items(), key=lambda item: item[1])}
         for name, metric in d.items():
             print(metric, name)
 
-    def __repr__(self):
+    def to_csv(self, filename='data.csv'):
+        '''method, behaviour, p, vse'''
+        header = ['method', 'behaviour', 'p', 'vse']
+        data = [header]
+        for method, value in zip(self.methods, self.vse):
+            data.append([method.name, method.behaviour, method.p, value])
+        np.savetxt(filename, data, fmt='%s', delimiter = ",")
+
+
+    def __str__(self):
         m = f'Simulation with {len(self.methods)} voting methods.\n'
         m += f'Voter model: {self.voter_model}\n'
         m += f'#Voters: {self.n_vot}\n'
