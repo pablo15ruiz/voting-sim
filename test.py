e783980b6e219314483ea2fdd02b16b134e98d45
Pablo Ruiz <57796883+pablo15ruiz@users.noreply.github.com>
2022-12-06 11:03:06 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,15 +3,16 @@ from voter_models import *
 from voting_methods import *
 
 import numpy as np
+import profile
 
 np.random.seed(1)
 
-n_vot = 1000
+n_vot = 300
 n_cand = 6
-n_iter = 1000
+n_iter = 300
 
 voter_model = ClusteredSpatialModel()
-# voter_model = ImpartialCulture()
+voter_model = ImpartialCulture()
 methods = [
     FPTP('honest'),
     FPTP('strategic'),
@@ -23,28 +24,29 @@ methods = [
     Borda('strategic', p=0.5),
     Borda('strategic1s'),
     Borda('strategic1s', p=0.5),
-    # IRV('honest'),
-    # IRV('strategic'),
-    # IRV('strategic', p=0.5),
-    # IRV('strategic1s'),
-    # IRV('strategic1s', p=0.5),
-    # Score('honest', max_score=1),
-    # Score('strategic', max_score=1),
-    # Score('strategic', max_score=1, p=0.5),
-    # Score('strategic1s', max_score=1),
-    # Score('strategic1s', max_score=1, p=0.5),
-    # Score('honest'),
-    # Score('strategic'),
-    # Score('strategic', p=0.5),
-    # Score('strategic1s'),
-    # Score('strategic1s', p=0.5),
-    # STAR('honest'),
-    # STAR('strategic'),
-    # STAR('strategic', p=0.5),
-    # STAR('strategic1s'),
-    # STAR('strategic1s', p=0.5)
+    IRV('honest'),
+    IRV('strategic'),
+    IRV('strategic', p=0.5),
+    IRV('strategic1s'),
+    IRV('strategic1s', p=0.5),
+    Score('honest', max_score=1),
+    Score('strategic', max_score=1),
+    Score('strategic', max_score=1, p=0.5),
+    Score('strategic1s', max_score=1),
+    Score('strategic1s', max_score=1, p=0.5),
+    Score('honest'),
+    Score('strategic'),
+    Score('strategic', p=0.5),
+    Score('strategic1s'),
+    Score('strategic1s', p=0.5),
+    STAR('honest'),
+    STAR('strategic'),
+    STAR('strategic', p=0.5),
+    STAR('strategic1s'),
+    STAR('strategic1s', p=0.5)
 ]
 
 sim = Simulation(n_vot, n_cand, n_iter, methods, voter_model)
 sim.simulate()
 sim.results()
+# sim.to_csv()
