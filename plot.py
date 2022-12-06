2f2e5f16b87696218e0afe0db079095f37a51f5a
Pablo Ruiz <57796883+pablo15ruiz@users.noreply.github.com>
2022-12-06 11:00:15 +0100

    Update plot.py

--- plot.py
+++ plot.py
@@ -4,11 +4,32 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv('data.csv')
-fig, ax = plt.subplots()
-plt.xlim(right=1)
+
+def func(row):
+    behaviour = row['behaviour']
+    if behaviour == 'strategic1s':
+        behaviour = 'one-sided strategic'
+    return f"{int(100*row['p'])}% {behaviour}"
+
+df['fancy'] = df.apply(func, axis=1)
+order = df.groupby('method')['vse'].max().sort_values(ascending=False).keys()
+
+fig, ax = plt.subplots(figsize=(10, 4))
 plt.xlabel('Voter Satisfaction Efficiency (%)')
-# ticks = np.arange(11)/10
-# labels = [f'{int(v*10)}%' for v in range(11)]
-# plt.xticks(ticks, labels)
-sns.stripplot(df, x='vse', y='method', jitter=False, ax=ax)
+plt.xticks(np.arange(11)/10, [f'{int(v*10)}%' for v in range(11)])
+plt.xlim(df['vse'].min() + df['vse'].max() - 1, 1)
+sns.stripplot(
+    df,
+    x='vse',
+    y='method',
+    hue='fancy',
+    jitter=False,
+    size=8,
+    order=order,
+    ax=ax
+)
+plt.grid()
+pos = ax.get_position()
+ax.set_position([pos.x0, pos.y0, pos.width * 0.8, pos.height])
+ax.legend(loc='center right', bbox_to_anchor=(1.4, 0.5))
 plt.show()
