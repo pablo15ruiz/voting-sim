f722ec130c0e91976a9728eb26650297169b1b3b
Pablo Ruiz <57796883+pablo15ruiz@users.noreply.github.com>
2022-12-13 18:48:34 +0100

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,36 +1,40 @@
 import string
+
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 class Election:
 
-    def __init__(self, n_vot, n_cand, util, poll=False):
+    def __init__(self, n_vot, n_cand):
         self.n_vot = n_vot
         self.n_cand = n_cand
-        self.util = util
-        self.poll = self.do_poll() if poll else None
-        self.cw = self.compute_condorcet_winner()
 
-    def compute_winner(self, method):
-        poll = self.poll if method.behaviour != 'honest' else None
-        ballots = method.compute_ballots(self.util, poll)
-        winner = method.compute_winner(ballots)
-        return winner
+    def compute_winners(self, methods, util):
+        winners = []
+        poll = None
+        if any([method.behaviour != 'honest' for method in methods]):
+            poll = self.compute_poll(util)
 
-    def compute_condorcet_winner(self):
-        # print(np.round(self.util, 3))
-        matrix = (self.util[:, None] < self.util[:, :, None]).astype(int).sum(axis=0)
+        for method in methods:
+            ballots = method.compute_ballots(util, poll)
+            winner = method.compute_winner(ballots)
+            winners.append(winner)
+
+        return winners
+
+    def compute_cycle(self, util):
+        matrix = (util[:, None] < util[:, :, None]).astype(int).sum(axis=0)
         count = (matrix >= int(self.n_vot/2)+1).sum(axis=1)
-        winner = count.argmax() if count.max() == self.n_cand-1 else None
-        return winner
+        cycle = True if count.max() != self.n_cand-1 else False
+        return cycle
 
-    def do_poll(self, threshold=0.7):
-        return (self.util > threshold).sum(axis=0).argsort()
+    def compute_poll(self, util, threshold=0.5):
+        return (util > threshold).sum(axis=0).argsort()
 
     def plot(self, voters, candidates):
         fig, ax = plt.subplots(figsize=(5, 5))
         ax.set_aspect('equal')
-        ax.set_xlim([-1, 2])
-        ax.set_ylim([-1, 2])
         ax.scatter(voters[:, 0], voters[:, 1], s=5)
         ax.scatter(candidates[:, 0], candidates[:, 1], s=70)
         names = list(string.ascii_uppercase)[:candidates.shape[0]]
@@ -49,33 +53,28 @@ class Simulation:
         self.methods = methods
         self.voter_model = voter_model
 
-        self.total_util = []
-        self.total_winners = []
+        self.vse = None
         self.cycle_freq = None
 
     def simulate(self):
         n_cycles = 0
         winners = []
-        poll = any([method.behaviour != 'honest' for method in self.methods])
+        util_sum = []
         for _ in range(self.n_iter):
             util = self.voter_model.generate_util(self.n_vot, self.n_cand)
-            self.total_util.append(util.sum(axis=0))
-            elec = Election(self.n_vot, self.n_cand, util, poll=poll)
-            n_cycles += elec.cw is None
-            w = [elec.compute_winner(method) for method in self.methods]
-            winners.append(w)
+            util_sum.append(util.sum(axis=0))
+            elec = Election(self.n_vot, self.n_cand)
+            n_cycles += elec.compute_cycle(util)
+            winners.append(elec.compute_winners(self.methods, util))
 
-        self.total_winners = np.array(winners)
-        self.total_util = np.array(self.total_util)
         self.cycle_freq = n_cycles/self.n_iter
+        winners = np.array(winners)
+        util_sum = np.array(util_sum)
 
-        self.compute_metrics()
-
-    def compute_metrics(self):
-        optimum = self.total_util.max(axis=1).mean()
+        optimum = util_sum.max(axis=1).mean()
         idx = np.tile(range(self.n_iter), reps=(len(self.methods), 1)).T
-        achieved = self.total_util[idx, self.total_winners].mean(axis=0)
-        average = self.total_util.mean()
+        achieved = util_sum[idx, winners].mean(axis=0)
+        average = util_sum.mean()
         self.vse = (achieved - average)/(optimum - average)
 
     def results(self):
@@ -87,11 +86,11 @@ class Simulation:
         for name, metric in d.items():
             print(metric, name)
 
-    def to_csv(self, filename='data.csv'):
-        '''method, behaviour, p, vse'''
-        header = ['method', 'behaviour', 'p', 'vse']
+    def to_csv(self, filename='vse.csv'):
+        header = ['method', 'behaviour', 'p', 'max_score', 'vse']
         data = [header]
         for method, value in zip(self.methods, self.vse):
+            # name = metho
             data.append([method.name, method.behaviour, method.p, value])
         np.savetxt(filename, data, fmt='%s', delimiter = ",")
 
@@ -99,7 +98,7 @@ class Simulation:
     def __str__(self):
         m = f'Simulation with {len(self.methods)} voting methods.\n'
         m += f'Voter model: {self.voter_model}\n'
-        m += f'#Voters: {self.n_vot}\n'
-        m += f'#Candidates: {self.n_cand}\n'
-        m += f'#Iterations: {self.n_iter}\n'
+        m += f'# Voters: {self.n_vot}\n'
+        m += f'# Candidates: {self.n_cand}\n'
+        m += f'# Iterations: {self.n_iter}\n'
         return m
