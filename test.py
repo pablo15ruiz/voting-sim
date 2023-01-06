3aa0e65f9027ea3be5f3baf6fba9970f93d264ea
Pablo Ruiz <57796883+pablo15ruiz@users.noreply.github.com>
2023-01-06 19:20:08 +0100

    Update test.py

--- test.py
+++ test.py
@@ -6,35 +6,14 @@ import numpy as np
 
 methods = [
     FPTP('honest'),
-    FPTP('strategic'),
-    FPTP('strategic', p=0.5),
-    FPTP('strategic1s'),
-    FPTP('strategic1s', p=0.5),
-    Borda('honest'),
     Borda('strategic'),
-    Borda('strategic', p=0.5),
-    Borda('strategic1s'),
-    Borda('strategic1s', p=0.5),
-    IRV('honest'),
-    IRV('strategic'),
     IRV('strategic', p=0.5),
-    IRV('strategic1s'),
-    IRV('strategic1s', p=0.5),
-    Score('honest', max_score=1),
-    Score('strategic', max_score=1),
-    Score('strategic', max_score=1, p=0.5),
     Score('strategic1s', max_score=1),
-    Score('strategic1s', max_score=1, p=0.5),
     Score('honest'),
-    Score('strategic'),
-    Score('strategic', p=0.5),
-    Score('strategic1s'),
-    Score('strategic1s', p=0.5),
     STAR('honest'),
-    STAR('strategic'),
-    STAR('strategic', p=0.5),
-    STAR('strategic1s'),
-    STAR('strategic1s', p=0.5)
+    ThreeTwoOne('strategic'),
+    MajorityJudgment('honest'),
+    SmithScore('strategic1s', p=0.3)
 ]
 
 np.random.seed(1)
